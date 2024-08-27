8a2723e7c75de322d4b716fb3229414c7503eb94
Tomer-Rubinstein <tomerrub11@gmail.com>
2024-08-27 14:35:10 +0300

    finished lyric parsing

--- src/utils/SubtitleParser.js
+++ src/utils/SubtitleParser.js
@@ -1,11 +1,13 @@
+
 function parseHtmlEnteties(str) {
     str = str.replace(/([0-9]{1,3});/gi, function(match, numStr) {
         var num = parseInt(numStr, 10);
         return String.fromCharCode(num);
     });
+    str = str.replace("lt;", "<");
+    str = str.replace("gt;", ">");
     return str;
 }
-
 
 export default function parseSubtitle(text) {
     var regex;
