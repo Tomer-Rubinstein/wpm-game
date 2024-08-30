f835c779099f421d1f336fe48a8c56d1f6623ddd
Tomer-Rubinstein <tomerrub11@gmail.com>
2024-08-30 13:03:21 +0300

    changed to a proper loading animation

--- src/components/LoadingComp.js
+++ src/components/LoadingComp.js
@@ -1,6 +1,16 @@
+import { CircularProgress } from "@mui/material";
 
 function LoadingComp() {
-    return "Loading...";
+    return (
+        <div style={{position: "relative", textAlign: "center"}}>
+            <CircularProgress
+                sx={(theme) => ({
+                    color: "#F5DEB3"
+                })}
+                size="5rem"
+            />
+        </div>
+    );
 }
 
 export default LoadingComp;
